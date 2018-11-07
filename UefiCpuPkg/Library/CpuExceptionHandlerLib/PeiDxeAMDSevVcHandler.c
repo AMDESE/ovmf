8bb8879edcae2fb11b28ed5c0287814cdb9f538a
Tom Lendacky <thomas.lendacky@amd.com>
2018-11-07 08:34:14 -0600

    Add base #VC exception handling support for Pei/Dxe phases
    An SEV-ES guest will generate a #VC exception when it encounters a
    non-automatic exit (NAE) event. It is expected that the #VC exception
    handler will communicate with the hypervisor using the GHCB to handle
    the NAE event.
    
    Update the Pei and Dxe exception handling support to recognize the #VC
    exception and call a common #VC handler.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/PeiDxeAMDSevVcHandler.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/PeiDxeAMDSevVcHandler.c
@@ -1,4 +1,7 @@
 
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Register/Amd/Msr.h>
 #include "CpuExceptionCommon.h"
 #include "AMDSevVcCommon.h"
 
@@ -7,5 +10,13 @@ DoVcException(
   EFI_SYSTEM_CONTEXT_X64 *Regs
   )
 {
-  return 0;
+  MSR_SEV_ES_GHCB_REGISTER  Msr;
+  GHCB                      *Ghcb;
+
+  Msr.GhcbPhysicalAddress = AsmReadMsr64 (MSR_SEV_ES_GHCB);
+  ASSERT(!Msr.Bits.GhcbNegotiateBit);
+
+  Ghcb = (GHCB *) Msr.GhcbPhysicalAddress;
+
+  return DoVcCommon (Ghcb, Regs);
 }
