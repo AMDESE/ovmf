5f02a22c746c882800c586e3a6c8ec762dcb91eb
Tom Lendacky <thomas.lendacky@amd.com>
2019-03-28 16:59:05 -0500

    Merge with sec-phase

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/SecAMDSevVcHandler.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/SecAMDSevVcHandler.c
@@ -16,14 +16,30 @@ DoVcException(
   GHCB                      *Ghcb;
 
   Msr.GhcbPhysicalAddress = AsmReadMsr64 (MSR_SEV_ES_GHCB);
+  Ghcb = (GHCB *) Msr.GhcbPhysicalAddress;
+
   if (Msr.Bits.GhcbNegotiateBit) {
+    if (Msr.GhcbProtocol.SevEsProtocolMin > Msr.GhcbProtocol.SevEsProtocolMax) {
+      ASSERT (0);
+      return GP_EXCEPTION;
+    }
+
+    if ((Msr.GhcbProtocol.SevEsProtocolMin > GHCB_VERSION_MAX) ||
+        (Msr.GhcbProtocol.SevEsProtocolMax < GHCB_VERSION_MIN)) {
+      ASSERT (0);
+      return GP_EXCEPTION;
+    }
+
     Msr.GhcbPhysicalAddress = GHCB_INIT;
     AsmWriteMsr64(MSR_SEV_ES_GHCB, Msr.GhcbPhysicalAddress);
 
-    SetMem ((VOID *) Msr.GhcbPhysicalAddress, sizeof (*Ghcb), 0);
-  }
+    Ghcb = (GHCB *) Msr.GhcbPhysicalAddress;
+    SetMem (Ghcb, sizeof (*Ghcb), 0);
 
-  Ghcb = (GHCB *) Msr.GhcbPhysicalAddress;
+    /* Set the version to the maximum that can be supported */
+    Ghcb->ProtocolVersion = MIN (Msr.GhcbProtocol.SevEsProtocolMax, GHCB_VERSION_MAX);
+    Ghcb->GhcbUsage = GHCB_STANDARD_USAGE;
+  }
 
   return DoVcCommon(Ghcb, Regs);
 }
