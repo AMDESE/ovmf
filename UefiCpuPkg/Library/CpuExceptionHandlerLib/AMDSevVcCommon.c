67a9a004f5a76d112bac8aca300d51f2db6c2a70
Tom Lendacky <thomas.lendacky@amd.com>
2019-05-17 16:33:54 -0500

    Add support for NPF NAE events (MMIO)
    Under SEV-ES, a NPF intercept for an NPT entry with a reserved bit set
    generates a #VC exception. This condition is assumed to be an MMIO access.
    VMGEXIT must be used to allow the hypervisor to handle this intercept.
    
    Add support to construct the required GHCB values to support a NPF NAE
    event for MMIO.  Parse the instruction that generated the #VC exception,
    setting the required register values in the GHCB and creating the proper
    SW_EXIT_INFO1, SW_EXITINFO2 and SW_SCRATCH values in the GHCB.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/AMDSevVcCommon.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/AMDSevVcCommon.c
@@ -125,6 +125,189 @@ UINTN
   );
 
 STATIC
+UINT64 *
+GetRegisterPointer (
+  EFI_SYSTEM_CONTEXT_X64   *Regs,
+  UINT8                    Register
+  )
+{
+  switch (Register) {
+  case 0:
+    return &Regs->Rax;
+  case 1:
+    return &Regs->Rcx;
+  case 2:
+    return &Regs->Rdx;
+  case 3:
+    return &Regs->Rbx;
+  case 4:
+    return &Regs->Rsp;
+  case 5:
+    return &Regs->Rbp;
+  case 6:
+    return &Regs->Rsi;
+  case 7:
+    return &Regs->Rdi;
+  case 8:
+    return &Regs->R8;
+  case 9:
+    return &Regs->R9;
+  case 10:
+    return &Regs->R10;
+  case 11:
+    return &Regs->R11;
+  case 12:
+    return &Regs->R12;
+  case 13:
+    return &Regs->R13;
+  case 14:
+    return &Regs->R14;
+  case 15:
+    return &Regs->R15;
+  }
+  ASSERT (0);
+
+  return 0;
+}
+
+STATIC
+VOID
+UpdateForDisplacement (
+  SEV_ES_INSTRUCTION_DATA  *InstructionData,
+  UINTN                    Size
+  )
+{
+  InstructionData->DisplacementSize = Size;
+  InstructionData->Immediate += Size;
+  InstructionData->End += Size;
+}
+
+STATIC
+BOOLEAN
+IsRipRelative (
+  SEV_ES_INSTRUCTION_DATA  *InstructionData
+  )
+{
+  SEV_ES_INSTRUCTION_OPCODE_EXT  *Ext = &InstructionData->Ext;
+
+  return ((InstructionData == LongMode64Bit) &&
+          (Ext->ModRm.Mod == 0) &&
+          (Ext->ModRm.Rm == 5)  &&
+          (InstructionData->SibPresent == FALSE));
+}
+
+STATIC
+UINTN
+GetEffectiveMemoryAddress (
+  EFI_SYSTEM_CONTEXT_X64   *Regs,
+  SEV_ES_INSTRUCTION_DATA  *InstructionData
+  )
+{
+  SEV_ES_INSTRUCTION_OPCODE_EXT  *Ext = &InstructionData->Ext;
+  UINTN                          EffectiveAddress = 0;
+
+  if (IsRipRelative (InstructionData)) {
+    /* RIP-relative displacement is a 32-bit signed value */
+    INT32 RipRelative = *(INT32 *) InstructionData->Displacement;
+
+    UpdateForDisplacement (InstructionData, 4);
+    return (UINTN) ((INTN) Regs->Rip + RipRelative);
+  }
+
+  switch (Ext->ModRm.Mod) {
+  case 1:
+  case 2:
+    switch (InstructionData->AddrSize) {
+    case Size8Bits:
+      UpdateForDisplacement (InstructionData, 1);
+      EffectiveAddress += (UINT8) (*(UINT8 *) (InstructionData->Displacement));
+      break;
+    case Size16Bits:
+      UpdateForDisplacement (InstructionData, 2);
+      EffectiveAddress += (UINT16) (*(UINT16 *) (InstructionData->Displacement));
+      break;
+    case Size32Bits:
+      UpdateForDisplacement (InstructionData, 4);
+      EffectiveAddress += (UINT32) (*(UINT32 *) (InstructionData->Displacement));
+      break;
+    case Size64Bits:
+      UpdateForDisplacement (InstructionData, 8);
+      EffectiveAddress += (UINT64) (*(UINT64 *) (InstructionData->Displacement));
+      break;
+    }
+    break;
+  }
+
+  if (InstructionData->SibPresent) {
+    switch (Ext->Sib.Index) {
+    case 4:
+      break;
+    default:
+      EffectiveAddress += (*GetRegisterPointer (Regs, Ext->Sib.Index) << Ext->Sib.Scale);
+    }
+
+    switch (Ext->Sib.Base) {
+    case 5:
+      if (Ext->ModRm.Mod) {
+        EffectiveAddress += *GetRegisterPointer (Regs, Ext->Sib.Base);
+      }
+      break;
+    default:
+      EffectiveAddress += *GetRegisterPointer (Regs, Ext->Sib.Base);
+    }
+  } else {
+    EffectiveAddress += *GetRegisterPointer (Regs, Ext->ModRm.Rm);
+  }
+
+  return EffectiveAddress;
+}
+
+STATIC
+VOID
+DecodeModRm (
+  EFI_SYSTEM_CONTEXT_X64   *Regs,
+  SEV_ES_INSTRUCTION_DATA  *InstructionData
+  )
+{
+  SEV_ES_INSTRUCTION_REX_PREFIX  *RexPrefix = &InstructionData->RexPrefix;
+  SEV_ES_INSTRUCTION_OPCODE_EXT  *Ext = &InstructionData->Ext;
+  SEV_ES_INSTRUCTION_MODRM       *ModRm = &InstructionData->ModRm;
+  SEV_ES_INSTRUCTION_SIB         *Sib = &InstructionData->Sib;
+
+  InstructionData->ModRmPresent = TRUE;
+  ModRm->Uint8 = *(InstructionData->End);
+
+  InstructionData->Displacement++;
+  InstructionData->Immediate++;
+  InstructionData->End++;
+
+  Ext->ModRm.Mod = ModRm->Bits.Mod;
+  Ext->ModRm.Reg = (RexPrefix->Bits.R << 3) | ModRm->Bits.Reg;
+  Ext->ModRm.Rm  = (RexPrefix->Bits.B << 3) | ModRm->Bits.Rm;
+
+  Ext->RegData = *GetRegisterPointer (Regs, Ext->ModRm.Reg);
+
+  if (Ext->ModRm.Mod == 3) {
+    Ext->RmData = *GetRegisterPointer (Regs, Ext->ModRm.Rm);
+  } else {
+    if (ModRm->Bits.Rm == 4) {
+      InstructionData->SibPresent = TRUE;
+      Sib->Uint8 = *(InstructionData->End);
+
+      InstructionData->Displacement++;
+      InstructionData->Immediate++;
+      InstructionData->End++;
+
+      Ext->Sib.Scale = Sib->Bits.Scale;
+      Ext->Sib.Index = (RexPrefix->Bits.X << 3) | Sib->Bits.Index;
+      Ext->Sib.Base  = (RexPrefix->Bits.B << 3) | Sib->Bits.Base;
+    }
+
+    Ext->RmData = GetEffectiveMemoryAddress (Regs, InstructionData);
+  }
+}
+
+STATIC
 VOID
 DecodePrefixes (
   EFI_SYSTEM_CONTEXT_X64   *Regs,
@@ -233,6 +416,91 @@ InitInstructionData (
   InstructionData->End = (UINT8 *) Regs->Rip;
 
   DecodePrefixes (Regs, InstructionData);
+}
+
+STATIC
+UINT64
+MmioExit (
+  GHCB                     *Ghcb,
+  EFI_SYSTEM_CONTEXT_X64   *Regs,
+  SEV_ES_INSTRUCTION_DATA  *InstructionData
+  )
+{
+  UINT64  ExitInfo1, ExitInfo2;
+  UINTN   Status;
+  UINTN   Bytes;
+  UINTN   *Register;
+
+  Bytes = 0;
+
+  switch (*(InstructionData->OpCodes)) {
+  /* MMIO write */
+  case 0x88:
+    Bytes = 1;
+  case 0x89:
+    DecodeModRm (Regs, InstructionData);
+    Bytes = (Bytes) ? Bytes
+                    : (InstructionData->DataSize == Size16Bits) ? 2
+                    : (InstructionData->DataSize == Size32Bits) ? 4
+                    : (InstructionData->DataSize == Size64Bits) ? 8
+                    : 0;
+
+    if (InstructionData->Ext.ModRm.Mod == 3) {
+      /* NPF on two register operands??? */
+      VmgExit (Ghcb, SvmExitUnsupported, SvmExitNpf, 0);
+      ASSERT (0);
+    }
+
+    ExitInfo1 = InstructionData->Ext.RmData;
+    ExitInfo2 = Bytes;
+    CopyMem (Ghcb->SharedBuffer, &InstructionData->Ext.RegData, Bytes);
+
+    Ghcb->SaveArea.SwScratch = (UINT64) Ghcb->SharedBuffer;
+    Status = VmgExit (Ghcb, SvmExitMmioWrite, ExitInfo1, ExitInfo2);
+    if (Status) {
+      return Status;
+    }
+    break;
+
+  /* MMIO read */
+  case 0x8A:
+    Bytes = 1;
+  case 0x8B:
+    DecodeModRm (Regs, InstructionData);
+    Bytes = (Bytes) ? Bytes
+                    : (InstructionData->DataSize == Size16Bits) ? 2
+                    : (InstructionData->DataSize == Size32Bits) ? 4
+                    : (InstructionData->DataSize == Size64Bits) ? 8
+                    : 0;
+    if (InstructionData->Ext.ModRm.Mod == 3) {
+      /* NPF on two register operands??? */
+      VmgExit (Ghcb, SvmExitUnsupported, SvmExitNpf, 0);
+      ASSERT (0);
+    }
+
+    ExitInfo1 = InstructionData->Ext.RmData;
+    ExitInfo2 = Bytes;
+
+    Ghcb->SaveArea.SwScratch = (UINT64) Ghcb->SharedBuffer;
+    Status = VmgExit (Ghcb, SvmExitMmioRead, ExitInfo1, ExitInfo2);
+    if (Status) {
+      return Status;
+    }
+
+    Register = GetRegisterPointer (Regs, InstructionData->Ext.ModRm.Reg);
+    if (Bytes == 4) {
+      /* Zero-extend for 32-bit operation */
+      *Register = 0;
+    }
+    CopyMem (Register, Ghcb->SharedBuffer, Bytes);
+    break;
+
+  default:
+    Status = GP_EXCEPTION;
+    ASSERT (0);
+  }
+
+  return Status;
 }
 
 STATIC
@@ -587,6 +855,10 @@ DoVcCommon(
 
   case SvmExitMsr:
     NaeExit = MsrExit;
+    break;
+
+  case SvmExitNpf:
+    NaeExit = MmioExit;
     break;
 
   default:
