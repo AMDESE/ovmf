b46aab6a483c6bda0107fb618348e77b146568fc
Tom Lendacky <thomas.lendacky@amd.com>
2019-08-13 08:58:22 -0500

    UefiCpuPkg/CpuExceptionHandler: Add base #VC exception handling support for Sec phase
    An SEV-ES guest will generate a #VC exception when it encounters a
    non-automatic exit (NAE) event. It is expected that the #VC exception
    handler will communicate with the hypervisor using the GHCB to handle
    the NAE event.
    
    NAE events can occur during the Sec phase, so initialize exception
    handling early in the OVMF Sec support. Add to the basic #VC exception
    handler to set the GHCB MSR to a pre-allocated GHCB and call a common
    #VC handler.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/AMDSevVcCommon.h
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/AMDSevVcCommon.h
@@ -3,9 +3,16 @@
 #define _AMD_SEV_VC_COMMON_H_
 
 #include <Protocol/DebugSupport.h>
+#include <Register/Amd/Ghcb.h>
 
 UINTN
 DoVcException(
+  EFI_SYSTEM_CONTEXT  Context
+  );
+
+UINTN
+DoVcCommon(
+  GHCB                *Ghcb,
   EFI_SYSTEM_CONTEXT  Context
   );
 
