5c8f36407e5ac7c7757ae5108cf861b36287a3ce
Tom Lendacky <thomas.lendacky@amd.com>
2019-11-19 16:18:13 -0600

    UefiCpuPkg: Implement library support for VMGEXIT
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=2198
    
    To support issuing a VMGEXIT instruction, create a library that can be
    used to perform GHCB and VMGEXIT related operations and to issue the
    actual VMGEXIT instruction when using the GHCB.
    
    Additionally, two VMGEXIT / MMIO related functions are created to support
    flash emulation. Flash emulation currently is done by marking the flash
    area as read-only and taking a nested page fault to perform the emulation
    of the instruction. However, emulation cannot be performed because there
    is no instruction decode assist support when SEV-ES is enabled. Provide
    routines to initiate an MMIO request to perform actual writes to flash.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

---
+++ UefiCpuPkg/Include/Library/VmgExitLib.h
@@ -0,0 +1,96 @@
+/** @file
+  Public header file for the VMGEXIT Support library class.
+
+  This library class defines some routines used when invoking the VMGEXIT
+  instruction in support of SEV-ES.
+
+  Copyright (c) 2019, Advanced Micro Devices, Inc. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __VMG_EXIT_LIB_H__
+#define __VMG_EXIT_LIB_H__
+
+#include <Register/Amd/Ghcb.h>
+
+
+/**
+  Perform VMGEXIT.
+
+  Sets the necessary fields of the GHCB, invokes the VMGEXIT instruction and
+  then handles the return actions.
+
+  @param[in]  GHCB       A pointer to the GHCB
+  @param[in]  ExitCode   VMGEXIT code to be assigned to the SwExitCode field of
+                         the GHCB.
+  @param[in]  ExitInfo1  VMGEXIT information to be assigned to the SwExitInfo1
+                         field of the GHCB.
+  @param[in]  ExitInfo2  VMGEXIT information to be assigned to the SwExitInfo2
+                         field of the GHCB.
+
+  @retval  0       VMGEXIT succeeded.
+  @retval  Others  VMGEXIT processing did not succeed. Exception number to
+                   be issued.
+
+**/
+UINTN
+EFIAPI
+VmgExit (
+  GHCB                *Ghcb,
+  UINT64              ExitCode,
+  UINT64              ExitInfo1,
+  UINT64              ExitInfo2
+  );
+
+/**
+  Perform pre-VMGEXIT initialization/preparation.
+
+  Performs the necessary steps in preparation for invoking VMGEXIT.
+
+  @param[in]  GHCB       A pointer to the GHCB
+
+**/
+VOID
+EFIAPI
+VmgInit (
+  GHCB                *Ghcb
+  );
+
+/**
+  Perform post-VMGEXIT cleanup.
+
+  Performs the necessary steps to cleanup after invoking VMGEXIT.
+
+  @param[in]  GHCB       A pointer to the GHCB
+
+**/
+VOID
+EFIAPI
+VmgDone (
+  GHCB                *Ghcb
+  );
+
+#define VMGMMIO_READ   False
+#define VMGMMIO_WRITE  True
+
+/**
+  Perform MMIO write of a buffer to a non-MMIO marked range.
+
+  Performs an MMIO write without taking a #VC. This is useful
+  for Flash devices, which are marked read-only.
+
+  @param[in]  UINT8      A pointer to the destination buffer
+  @param[in]  UINTN      The immediate value to write
+  @param[in]  UINTN      Number of bytes to write
+
+**/
+VOID
+EFIAPI
+VmgMmioWrite (
+  UINT8               *Dest,
+  UINT8               *Src,
+  UINTN                Bytes
+  );
+
+#endif
---
+++ UefiCpuPkg/Library/VmgExitLib/VmgExitLib.c
@@ -0,0 +1,132 @@
+/** @file
+  VMGEXIT Support Library.
+
+  Copyright (c) 2019, Advanced Micro Devices, Inc. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Base.h>
+#include <Uefi.h>
+#include <Library/BaseMemoryLib.h>
+#include <Register/Amd/Ghcb.h>
+#include <Register/Amd/Msr.h>
+
+STATIC
+UINTN
+VmgExitErrorCheck (
+  GHCB                *Ghcb
+  )
+{
+  GHCB_EXIT_INFO   ExitInfo;
+  UINTN            Reason, Action;
+
+  if (!Ghcb->SaveArea.SwExitInfo1) {
+    return 0;
+  }
+
+  ExitInfo.Uint64 = Ghcb->SaveArea.SwExitInfo1;
+  Action = ExitInfo.Elements.Lower32Bits;
+  if (Action == 1) {
+    Reason = ExitInfo.Elements.Upper32Bits;
+
+    switch (Reason) {
+    case UD_EXCEPTION:
+    case GP_EXCEPTION:
+      return Reason;
+    }
+  }
+
+  ASSERT (0);
+  return GP_EXCEPTION;
+}
+
+UINTN
+EFIAPI
+VmgExit (
+  GHCB                *Ghcb,
+  UINT64              ExitCode,
+  UINT64              ExitInfo1,
+  UINT64              ExitInfo2
+  )
+{
+  Ghcb->SaveArea.SwExitCode = ExitCode;
+  Ghcb->SaveArea.SwExitInfo1 = ExitInfo1;
+  Ghcb->SaveArea.SwExitInfo2 = ExitInfo2;
+  AsmVmgExit ();
+
+  return VmgExitErrorCheck (Ghcb);
+}
+
+VOID
+EFIAPI
+VmgInit (
+  GHCB                *Ghcb
+  )
+{
+  SetMem (&Ghcb->SaveArea, sizeof (Ghcb->SaveArea), 0);
+}
+
+VOID
+EFIAPI
+VmgDone (
+  GHCB                *Ghcb
+  )
+{
+}
+
+UINTN
+EFIAPI
+VmgMmio (
+  UINT8               *MmioAddress,
+  UINT8               *Buffer,
+  UINTN               Bytes,
+  BOOLEAN             Write
+  )
+{
+  UINT64                    MmioOp;
+  UINT64                    ExitInfo1, ExitInfo2;
+  UINTN                     Status;
+  GHCB                      *Ghcb;
+  MSR_SEV_ES_GHCB_REGISTER  Msr;
+
+  Msr.GhcbPhysicalAddress = AsmReadMsr64 (MSR_SEV_ES_GHCB);
+  Ghcb = Msr.Ghcb;
+
+  if (Write) {
+    MmioOp = SvmExitMmioWrite;
+  } else {
+    MmioOp = SvmExitMmioRead;
+  }
+
+  ExitInfo1 = (UINT64) (UINTN) MmioAddress;
+  ExitInfo2 = Bytes;
+
+  if (Write) {
+    CopyMem (Ghcb->SharedBuffer, Buffer, Bytes);
+  }
+
+  Ghcb->SaveArea.SwScratch = (UINT64) (UINTN) Ghcb->SharedBuffer;
+  Status = VmgExit (Ghcb, MmioOp, ExitInfo1, ExitInfo2);
+  if (Status != 0) {
+    return Status;
+  }
+
+  if (!Write) {
+    CopyMem (Buffer, Ghcb->SharedBuffer, Bytes);
+  }
+
+  return 0;
+}
+
+VOID
+EFIAPI
+VmgMmioWrite (
+  UINT8               *Dest,
+  UINT8               *Src,
+  UINTN                Bytes
+  )
+{
+  VmgMmio (Dest, Src, Bytes, TRUE);
+}
+
