39e5528af8aeeab437941691c1bc85d9b0ade353
Tom Lendacky <thomas.lendacky@amd.com>
2020-02-05 08:12:57 -0600

    UefiCpuPkg/CpuExceptionHandler: Add support for MWAIT/MWAITX NAE events
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=2198
    
    Under SEV-ES, a MWAIT/MWAITX intercept generates a #VC exception.
    VMGEXIT must be used to allow the hypervisor to handle this intercept.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/X64/ArchAMDSevVcHandler.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/X64/ArchAMDSevVcHandler.c
@@ -593,6 +593,31 @@ MmioExit (
 
 STATIC
 UINT64
+MwaitExit (
+  GHCB                     *Ghcb,
+  EFI_SYSTEM_CONTEXT_X64   *Regs,
+  SEV_ES_INSTRUCTION_DATA  *InstructionData
+  )
+{
+  UINT64  Status;
+
+  DecodeModRm (Regs, InstructionData);
+
+  Ghcb->SaveArea.Rax = Regs->Rax;
+  GhcbSetRegValid (Ghcb, GhcbRax);
+  Ghcb->SaveArea.Rcx = Regs->Rcx;
+  GhcbSetRegValid (Ghcb, GhcbRcx);
+
+  Status = VmgExit (Ghcb, SvmExitMwait, 0, 0);
+  if (Status) {
+    return Status;
+  }
+
+  return 0;
+}
+
+STATIC
+UINT64
 MonitorExit (
   GHCB                     *Ghcb,
   EFI_SYSTEM_CONTEXT_X64   *Regs,
@@ -1105,6 +1130,10 @@ DoVcCommon (
 
   case SvmExitMonitor:
     NaeExit = MonitorExit;
+    break;
+
+  case SvmExitMwait:
+    NaeExit = MwaitExit;
     break;
 
   case SvmExitNpf:
