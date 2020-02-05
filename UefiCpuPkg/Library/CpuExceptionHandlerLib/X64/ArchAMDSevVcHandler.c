a3b2b7560b9efb4894579513ff1af1dda843c4ca
Tom Lendacky <thomas.lendacky@amd.com>
2020-02-05 08:12:57 -0600

    UefiCpuPkg/CpuExceptionHandler: Add support for RDTSCP NAE events
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=2198
    
    Under SEV-ES, a RDTSCP intercept generates a #VC exception. VMGEXIT must be
    used to allow the hypervisor to handle this intercept.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/X64/ArchAMDSevVcHandler.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/X64/ArchAMDSevVcHandler.c
@@ -611,6 +611,35 @@ WbinvdExit (
 
 STATIC
 UINT64
+RdtscpExit (
+  GHCB                     *Ghcb,
+  EFI_SYSTEM_CONTEXT_X64   *Regs,
+  SEV_ES_INSTRUCTION_DATA  *InstructionData
+  )
+{
+  UINT64  Status;
+
+  DecodeModRm (Regs, InstructionData);
+
+  Status = VmgExit (Ghcb, SvmExitRdtscp, 0, 0);
+  if (Status) {
+    return Status;
+  }
+
+  if (!GhcbIsRegValid (Ghcb, GhcbRax) ||
+      !GhcbIsRegValid (Ghcb, GhcbRcx) ||
+      !GhcbIsRegValid (Ghcb, GhcbRdx)) {
+    return UnsupportedExit (Ghcb, Regs, InstructionData);
+  }
+  Regs->Rax = Ghcb->SaveArea.Rax;
+  Regs->Rcx = Ghcb->SaveArea.Rcx;
+  Regs->Rdx = Ghcb->SaveArea.Rdx;
+
+  return 0;
+}
+
+STATIC
+UINT64
 VmmCallExit (
   GHCB                     *Ghcb,
   EFI_SYSTEM_CONTEXT_X64   *Regs,
@@ -1037,6 +1066,10 @@ DoVcCommon (
 
   case SvmExitVmmCall:
     NaeExit = VmmCallExit;
+    break;
+
+  case SvmExitRdtscp:
+    NaeExit = RdtscpExit;
     break;
 
   case SvmExitWbinvd:
