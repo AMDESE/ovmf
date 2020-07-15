0d091f43ae96ca4617f6a95a018bfaed6280eec2
Tom Lendacky <thomas.lendacky@amd.com>
2020-07-15 08:22:13 -0500

    MdePkg/BaseLib: Add support for the XGETBV instruction
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=2198
    
    Under SEV-ES, a CPUID instruction requires the current value of the XCR0
    register. In order to retrieve that value, the XGETBV instruction needs
    to be executed.
    
    Provide the necessary support to execute the XGETBV instruction.
    
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- MdePkg/Library/BaseLib/Ia32/GccInline.c
+++ MdePkg/Library/BaseLib/Ia32/GccInline.c
@@ -584,3 +584,31 @@ AsmReadTsc (
 
   return Data;
 }
+
+
+/**
+  Executes a XGETBV instruction
+
+  Executes a XGETBV instruction. This function is only available on IA-32 and
+  x64.
+
+  @param[in] Index        Extended control register index
+
+  @return                 The current value of the extended control register
+**/
+UINT64
+EFIAPI
+AsmXGetBv (
+  IN UINT32 Index
+  )
+{
+  UINT64 Data;
+
+  __asm__ __volatile__ (
+    "xgetbv"
+    : "=A" (Data)
+    : "c"  (Index)
+    );
+
+  return Data;
+}
