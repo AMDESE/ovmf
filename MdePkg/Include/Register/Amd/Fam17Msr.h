dd6b2bcb115e7e2de7c449efd97dbfc34ee7e556
Brijesh Singh <brijesh.singh@amd.com>
2021-04-08 22:52:22 -0500

    MdePkg: Define the GHCB GPA structure
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=3275
    
    An SEV-SNP guest is required to perform the GHCB GPA registration. See
    the GHCB specification for further details.
    
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Min Xu <min.m.xu@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- MdePkg/Include/Register/Amd/Fam17Msr.h
+++ MdePkg/Include/Register/Amd/Fam17Msr.h
@@ -53,18 +53,25 @@ typedef union {
     UINT64  Features:52;
   } GhcbHypervisorFeatures;
 
+  struct {
+    UINT64  Function:12;
+    UINT64  GuestFrameNumber:52;
+  } GhcbGpaRegister;
+
   VOID    *Ghcb;
 
   UINT64  GhcbPhysicalAddress;
 } MSR_SEV_ES_GHCB_REGISTER;
 
-#define GHCB_INFO_SEV_INFO                 1
-#define GHCB_INFO_SEV_INFO_GET             2
-#define GHCB_INFO_CPUID_REQUEST            4
-#define GHCB_INFO_CPUID_RESPONSE           5
-#define GHCB_HYPERVISOR_FEATURES_REQUEST   128
-#define GHCB_HYPERVISOR_FEATURES_RESPONSE  129
-#define GHCB_INFO_TERMINATE_REQUEST        256
+#define GHCB_INFO_SEV_INFO                    1
+#define GHCB_INFO_SEV_INFO_GET                2
+#define GHCB_INFO_CPUID_REQUEST               4
+#define GHCB_INFO_CPUID_RESPONSE              5
+#define GHCB_INFO_GHCB_GPA_REGISTER_REQUEST   18
+#define GHCB_INFO_GHCB_GPA_REGISTER_RESPONSE  19
+#define GHCB_HYPERVISOR_FEATURES_REQUEST      128
+#define GHCB_HYPERVISOR_FEATURES_RESPONSE     129
+#define GHCB_INFO_TERMINATE_REQUEST           256
 
 #define GHCB_TERMINATE_GHCB                0
 #define GHCB_TERMINATE_GHCB_GENERAL        0
