ddba55094e259f3ceb027d616000f3cf98e8b204
Tom Lendacky <thomas.lendacky@amd.com>
2023-03-02 13:42:35 -0600

    OvmfPkg/CcExitLib: Add support for the SVSM_CORE_PVALIDATE call
    The PVALIDATE instruction can only be performed at VMPL0. An SVSM will
    be present when running at VMPL1 or lower.
    
    When an SVSM is present, use the SVSM_CORE_PVALIDATE call to perform
    memory validation instead of issuing the PVALIDATE instruction directly.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- OvmfPkg/Library/CcExitLib/CcExitSvsm.h
+++ OvmfPkg/Library/CcExitLib/CcExitSvsm.h
@@ -39,4 +39,51 @@ typedef PACKED struct {
   UINT8  SvsmBuffer[SIZE_4KB - 8];
 } SVSM_CAA;
 
+#define SVSM_SUCCESS                           0x00000000
+#define SVSM_ERR_INCOMPLETE                    0x80000000
+#define SVSM_ERR_UNSUPPORTED_PROTOCOL          0x80000001
+#define SVSM_ERR_UNSUPPORTED_CALL              0x80000002
+#define SVSM_ERR_INVALID_ADDRESS               0x80000003
+#define SVSM_ERR_INVALID_FORMAT                0x80000004
+#define SVSM_ERR_INVALID_PARAMETER             0x80000005
+#define SVSM_ERR_INVALID_REQUEST               0x80000006
+#define SVSM_ERR_BUSY                          0x80000007
+
+#define SVSM_ERR_PVALIDATE_FAIL_INPUT          0x80001001
+#define SVSM_ERR_PVALIDATE_FAIL_SIZE_MISMATCH  0x80001006
+#define SVSM_ERR_PVALIDATE_FAIL_NO_CHANGE      0x80001010
+
+typedef union {
+  struct {
+    UINT64  PageSize   : 2;
+    UINT64  Action     : 1;
+    UINT64  IgnoreCf   : 1;
+    UINT64  Reserved_2 : 8;
+    UINT64  Address    : 52;
+  };
+  UINT64  Uint64;
+} SVSM_PVALIDATE_ENTRY;
+
+typedef PACKED struct {
+  UINT16  Entries;
+  UINT16  Next;
+
+  UINT8   Reserved_1[4];
+
+  SVSM_PVALIDATE_ENTRY  Entry[];
+} SVSM_PVALIDATE_REQUEST;
+
+typedef union {
+  SVSM_PVALIDATE_REQUEST  PvalidateRequest;
+} SVSM_REQUEST;
+
+typedef union {
+  struct {
+    UINT32  CallId;
+    UINT32  Protocol;
+  };
+
+  UINT64  Uint64;
+} SVSM_FUNCTION;
+
 #endif
